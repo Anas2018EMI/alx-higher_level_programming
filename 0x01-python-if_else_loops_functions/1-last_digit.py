2b9d85e1e53b7bb203e807bb2d52071f35774a59
Anas2018EMI <anassaatef@gmail.com>
2024-09-10 04:37:59 -0700

    Initial commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l_digit = number % 10
+if number >= 0:
+    l_digit = number % 10
+else:
+    l_digit = -(10 - number % 10)
+
 if l_digit == 0:
     str = "and is 0"
 elif l_digit < 6:
