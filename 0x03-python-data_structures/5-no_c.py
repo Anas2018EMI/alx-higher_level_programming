56a6ed4a53b92ef18fb1b430c0ae30cfd145a44e
Anas2018EMI <anassaatef@gmail.com>
2024-09-14 03:20:26 -0700

    Initial commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    a = list(my_string)
-    if "c" in a:
-        a.remove("c")
-    if "C" in a:
-        a.remove("C")
-    s = ""
-    for chr in a:
-        s += chr
+    if not my_string:
+        pass
+    else:
+        a = list(my_string)
+        if "c" in a:
+            a.remove("c")
+        if "C" in a:
+            a.remove("C")
+        s = ""
+        for chr in a:
+            s += chr
 
-    return s
+        return s
