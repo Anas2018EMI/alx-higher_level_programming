d0f8df1d18cf42b0c59b0165bd69917f7a9d7f1f
Anas2018EMI <anassaatef@gmail.com>
2024-10-01 15:54:29 +0100

    Initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""_summary_
+    Defining the Square class
+    """
+
+
+class Square:
+    """_summary_
+    Square is a class in Python
+    """
+    def __init__(self, size) -> None:
+        """_summary_
+        Instantiation of the class objects
+        Args:
+            size (int): size of a square
+        """
+        self.__size = size
