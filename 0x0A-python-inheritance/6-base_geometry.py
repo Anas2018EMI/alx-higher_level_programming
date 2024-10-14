fcc4f6f25adbfef8f035b1cb22f2cd174e60952f
Anas2018EMI <anassaatef@gmail.com>
2024-10-14 20:40:46 +0100

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""raises an Exception with the message area() is not
+implemented
+
+    Raises:
+        Exception: _description_
+    """
+
+
+class BaseGeometry:
+    def area(self):
+        """raises an Exception with the message area() is
+        not implemented
+
+        Raises:
+            Exception: _description_
+        """
+        raise Exception("area() is not implemented")
