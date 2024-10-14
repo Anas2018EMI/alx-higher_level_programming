b9c9a14fc22985b564466e5799f5a3b90b02388e
Anas2018EMI <anassaatef@gmail.com>
2024-10-14 19:02:07 +0100

    Initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" returns the list of available attributes
+and methods of an object
+    """
+
+
+def lookup(obj):
+    """ returns the list of available attributes and
+    methods of an object
+
+    Args:
+        obj (_type_): First arg
+
+    Returns:
+        list: list of available attributes
+and methods of an object
+    """
+    return dir(obj)
