e5825170a926d4812118e0b31ce6f2ae8e0c2319
Anas2018EMI <anassaatef@gmail.com>
2024-10-15 15:06:00 +0100

    Initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" reads a text file (UTF8) and prints it to stdout
+    """
+
+
+def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """
+    with open(filename) as file:
+        content = file.read()
+
+    print(content)
