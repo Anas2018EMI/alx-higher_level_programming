75b75556b3826f8e88fec8b61b9380e8ae0eb14b
Anas2018EMI <anassaatef@gmail.com>
2024-10-15 18:37:51 +0100

    Initial commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Defines a student."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {at: getattr(self, at) for at in attrs if hasattr(self, at)}
