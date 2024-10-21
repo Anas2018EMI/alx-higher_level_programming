7fe1ab9662f375c580162401a72c74346cfe1b4c
Anas2018EMI <anassaatef@gmail.com>
2024-10-21 22:08:21 +0100

    Initial commit

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,82 +1,100 @@
 #!/usr/bin/python3
-"""Defines unittests for base.py.
-
-Unittest classes:
-    TestBase_instantiation
-"""
+"""Unittest for Base class"""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import os
 
 
-class TestBase_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Base class."""
+class TestBase(unittest.TestCase):
+    """Test cases for the Base class"""
 
-    def test_no_args_instantiates(self):
-        self.assertEqual(Base(), Base(1))
+    def setUp(self):
+        """Reset __nb_objects before each test"""
+        Base._Base__nb_objects = 0
 
-    def test_two_args(self):
-        with self.assertRaises(TypeError):
-            Base(1, 2)
-
-    def test_nb_instances_after_unique_id(self):
+    def test_id_assignment(self):
+        """Test id assignment"""
         b1 = Base()
-        b2 = Base(12)
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 1)
+        self.assertEqual(b1.id, 1)
 
-    def test_id_is_public(self):
-        b = Base(12)
-        b.id = 15
-        self.assertEqual(15, b.id)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
 
-    def test_nb_instances_private(self):
-        with self.assertRaises(AttributeError):
-            print(Base(12).__nb_instances)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
 
-    def test_str_id(self):
-        self.assertEqual("hello", Base("hello").id)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
 
-    def test_float_id(self):
-        self.assertEqual(5.5, Base(5.5).id)
+    def test_to_json_string(self):
+        """Test to_json_string method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertTrue(isinstance(json_dictionary, str))
+        self.assertEqual(json_dictionary, '[{"x": 2, "y": 8, "id": 1, "height": 7, "width": 10}]')
 
-    def test_complex_id(self):
-        self.assertEqual(complex(5), Base(complex(5)).id)
+    def test_save_to_file(self):
+        """Test save_to_file method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            self.assertTrue(len(file.read()) > 0)
+        os.remove("Rectangle.json")
 
-    def test_dict_id(self):
-        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
+    def test_from_json_string(self):
+        """Test from_json_string method"""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4}, 
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertTrue(isinstance(list_output, list))
+        self.assertEqual(list_output, list_input)
 
-    def test_bool_id(self):
-        self.assertEqual(True, Base(True).id)
+    def test_create(self):
+        """Test create method"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertIsNot(r1, r2)
+        self.assertEqual(str(r1), str(r2))
 
-    def test_list_id(self):
-        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
+    def test_load_from_file(self):
+        """Test load_from_file method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertTrue(all(isinstance(obj, Rectangle) for obj in list_rectangles_output))
+        self.assertEqual(len(list_rectangles_output), 2)
+        os.remove("Rectangle.json")
 
-    def test_tuple_id(self):
-        self.assertEqual((1, 2), Base((1, 2)).id)
+    def test_save_to_file_csv(self):
+        """Test save_to_file_csv method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file_csv([r1, r2])
+        with open("Rectangle.csv", "r") as file:
+            self.assertTrue(len(file.read()) > 0)
+        os.remove("Rectangle.csv")
 
-    def test_set_id(self):
-        self.assertEqual({1, 2, 3, 4}, Base({1, 2, 3, 4}).id)
+    def test_load_from_file_csv(self):
+        """Test load_from_file_csv method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file_csv(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file_csv()
+        self.assertTrue(all(isinstance(obj, Rectangle) for obj in list_rectangles_output))
+        self.assertEqual(len(list_rectangles_output), 2)
+        os.remove("Rectangle.csv")
 
-    def test_frozenset_id(self):
-        self.assertEqual(frozenset({1, 2, 3, 4}), Base(frozenset({1, 2, 3, 4})).id)
 
-    def test_range_id(self):
-        self.assertEqual(range(5), Base(range(5)).id)
-
-    def test_bytes_id(self):
-        self.assertEqual(b'Python', Base(b'Python').id)
-
-    def test_bytearray_id(self):
-        self.assertEqual(bytearray(b'abcefg'), Base(bytearray(b'abcefg')).id)
-
-    def test_memoryview_id(self):
-        self.assertEqual(memoryview(b'abcefg'), Base(memoryview(b'abcefg')).id)
-
-    def test_inf_id(self):
-        self.assertEqual(float('inf'), Base(float('inf')).id)
-
-    def test_nan_id(self):
-        self.assertNotEqual(float('nan'), Base(float('nan')).id)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
