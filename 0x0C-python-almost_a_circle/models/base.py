bc4b6c604c7cc8caa90b6c1b656e91bed20e957c
Anas2018EMI <anassaatef@gmail.com>
2024-10-21 21:46:59 +0100

    Initial commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
     """
 
 import json
+import os
 
 
 class Base:
@@ -50,3 +51,64 @@ class Base:
         content = cls.to_json_string(content0)
         with open(f"{cls.__name__}.json", mode="w") as file:
             file.write(content)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Return the list of the JSON string representation json_string.
+
+        Args:
+            json_string (str): A string representing a list of dictionaries.
+
+        Returns:
+            list: The list represented by json_string.
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Create an instance with all attributes already set.
+
+        Args:
+            **dictionary: Double pointer to a dictionary
+            containing attribute values.
+
+        Returns:
+            An instance of the class with attributes set
+            according to the dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            return None
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Return a list of instances from a file.
+
+        The filename must be: <Class name>.json - example: Rectangle.json
+        If the file doesn't exist, return an empty list.
+        Otherwise, return a list of instances - the type of these instances
+        depends on cls (current class using this method)
+
+        Returns:
+            list: A list of instances.
+        """
+        filename = cls.__name__ + ".json"
+        if not os.path.exists(filename):
+            return []
+
+        with open(filename, 'r') as file:
+            json_string = file.read()
+
+        list_dicts = cls.from_json_string(json_string)
+        return [cls.create(**d) for d in list_dicts]
