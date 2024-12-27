5dc90d7c46fc8db6862654c141e0032a25eb31fc
Anas2018EMI <anassaatef@gmail.com>
2024-12-27 13:39:01 -0800

    Initial commit

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(
         """SELECT cities.name FROM cities JOIN states
-        ON cities.state_id = states.id WHERE states.name = %s
+        ON cities.state_id = states.id WHERE BINARY states.name = %s
         ORDER BY cities.id
 """, (argv[4],))
     rows = cur.fetchall()
