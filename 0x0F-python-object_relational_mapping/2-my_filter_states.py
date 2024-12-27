65c027b3449b536fdc9eab774bf4f8b90bbc2872
Anas2018EMI <anassaatef@gmail.com>
2024-12-27 12:19:27 -0800

    Initial commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name='{}' ORDER BY id".format(argv[4]))
+        """SELECT * FROM states WHERE BINARY name='{}'
+        ORDER BY id""".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
