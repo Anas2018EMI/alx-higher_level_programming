4fa422ffcd0b96eac54892add78d874ea5d5f4ac
Anas2018EMI <anassaatef@gmail.com>
2024-12-27 12:00:01 -0800

    Initial commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,49 @@
 #!/usr/bin/python3
-"""  a script that lists all states with a name starting with N
-    (upper N) from the database hbtn_0e_0_usa
+"""
+    all states with a name starting with N from the hbtn_0e_0_usa.
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == "__main__":
+
+def list_states_starting_with_N(username, password, database):
+    """
+    Lists all states with a name starting with N from the db hbtn_0e_0_usa
+    """
+    # Connect to the MySQL server
     db = MySQLdb.connect(
-        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-    rows = cur.fetchall()
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to select states starting with 'N'
+    query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Print the results
     for row in rows:
         print(row)
-    cur.close()
+
+    # Close the cursor and the database connection
+    cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    # Get the command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the function to list states
+    list_states_starting_with_N(username, password, database)
